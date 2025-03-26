96c0fee74ec3756c8c81e696bc33777309bf1378
VitusPuttmann <vitus.puttmann@outlook.com>
2025-03-26 21:46:50 +0100

    Improve code day 1

--- src/advent_code_2024/day_01.py
+++ src/advent_code_2024/day_01.py
@@ -1,12 +1,13 @@
-""" This module contains functions that solve the challenges of day 1
-    of the advent of code 2024 (https://adventofcode.com/).
+"""
+This module contains functions that solve the challenges of day 1 of the
+Advent of Code 2024 (https://adventofcode.com/).
 """
 
 
-import re
 import os
 import time
-import inspect
+
+from collections import Counter
 
 
 """ Prepare filepath for reading in input. """
@@ -27,72 +28,47 @@ def calc_runtime(func):
     return wrapper
 
 
-def prep_lists(filepath):
-    """ Read a text file with two columns and create a list for each
-        column. """
+def prep_lists(filepath: str):
+    """ Read a text file with two columns and create a list for each column. """
     
-    fin = open(filepath, 'r')
-
     list_1 = []
     list_2 = []
-    for line in fin:
-        str_list = str.split(line, '   ')
-        entry_1 = int(str_list[0])
-        entry_2 = int(re.sub('\n', '', str_list[1]))
-        list_1.append(entry_1)
-        list_2.append(entry_2)
     
-    fin.close()
+    with open(filepath, 'r') as file_input:
+        for line in file_input:
+            line = line.strip()
+            str_list = line.split()
+            list_1.append(int(str_list[0]))
+            list_2.append(int(str_list[1]))
     
     return list_1, list_2
 
 
-def create_dict(val_list):
-    """ Take a list and create a dictionary containing its unique elements
-        and their frequencies. """
-    
-    val_dict = {}
-    for num_val in val_list:
-        if not num_val in val_dict:
-            val_dict[num_val] = 1
-        else:
-            val_dict[num_val] += 1
-    
-    return val_dict
-
-
 @calc_runtime
-def solve_puzzle_01_01(filepath):
-    """ Solve the first puzzle of day 1 by calculating the distance
-        between two lists. """
+def solve_puzzle_01_01(filepath: str) -> int:
+    """ Calculate the distance between two lists. """
     
     list_1, list_2 = prep_lists(filepath)
     
     list_1.sort()
     list_2.sort()
     
-    res_acc = 0
-    for val in range(len(list_1)):
-        res_acc += abs(list_1[val] - list_2[val])
-    
-    return res_acc
+    distance = sum(abs(first - second) for first, second in zip(list_1, list_2))
+
+    return distance
 
 
 @calc_runtime
-def solve_puzzle_01_02(filepath):
-    """ Solve the second puzzle of day 1 by calculating the
-        similarity score of two lists. """
+def solve_puzzle_01_02(filepath: str) -> int:
+    """Calculate the similarity score of two lists. """
     
     list_1, list_2 = prep_lists(filepath)
 
-    list_dict = create_dict(list_2)
+    second_counts = Counter(list_2)
 
-    res_score = 0
-    for list_val in list_1:
-        if list_val in list_dict:
-            res_score += (list_val * int(list_dict[list_val]))
+    similarity = sum(num * second_counts[num] for num in list_1)
     
-    return res_score
+    return similarity
 
 
 def main():
