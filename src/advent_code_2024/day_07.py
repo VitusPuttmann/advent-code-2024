f4f50409f54279290325674f4bd8c643637c4010
VitusPuttmann <vitus.puttmann@outlook.com>
2025-04-02 21:04:34 +0200

    Improve code day 7

--- src/advent_code_2024/day_07.py
+++ src/advent_code_2024/day_07.py
@@ -1,8 +1,11 @@
-""" This module contains functions that solve the challenges of day 7
-    of the advent of code 2024 (https://adventofcode.com/).
+"""
+This module contains functions that solve the challenges of day 7 of the
+Advent of Code 2024 (https://adventofcode.com/).
 """
 
+
 import os
+import time
 
 
 """ Prepare filepath for reading in input. """
@@ -11,28 +14,33 @@ parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
 filepath = os.path.join(parent_dir, "data", "day_07_input.txt")
 
 
-def list_equations(filepath):
-    """ Read a text file and create a list consisting of
-        its equations. """
-    
-    fin = open(filepath, 'r')
+def calc_runtime(func):
+    """ Measure runtime of function. """
+
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        runtime = round(end_time - start_time, 5)
+        return result, runtime
+    return wrapper
+
+
+def list_equations(filepath: str) -> list[list[int]]:
+    """ Read a text file and create a list consisting of its equations. """
 
     equations_list = []
-    for line in fin:
-        str_line = line.strip()
-        str_list = str.split(str_line, ':')
-        list_first = [int(str_list[0])]
-        list_second = str_list[1].strip()
-        list_second = list_second.strip()
-        list_second = str.split(list_second, " ")
-        list_second = [int(x) for x in list_second]
-        list_combined =  list_first + list_second
-        equations_list.append(list_combined)
-    
+    with open(filepath, 'r') as file_input:
+        for line in file_input:
+            str_line = line.strip()
+            str_line = str_line.replace(':', '')
+            int_line = [int(x) for x in str_line.split()]
+            equations_list.append(int_line)
+
     return equations_list
 
 
-def calculate_outcomes(int_list, concat=False):
+def calculate_outcomes(int_list: list[int], concat: bool = False):
     """ Calculate all potential outcomes of sequentially combining the values
         of a list via addition or multiplication or via the addition of
         concatenation. """
@@ -59,9 +67,12 @@ def calculate_outcomes(int_list, concat=False):
     return outcomes_list
 
 
-def solve_puzzle_07_01(filepath):
-    """ Create a list from a text file, identify all possible outcomes, 
-        and calculate their sum. """
+@calc_runtime
+def solve_puzzle_07(filepath: str, second: bool = False) -> int:
+    """ Create a list of lists from a text file, separate the outcome from the
+        list, calculate all possible results for the remaining numbers, check
+        whether the outcome is among the results, and calculate sum of results
+        for which this is the case. """
     
     equations_list = list_equations(filepath)
 
@@ -69,24 +80,7 @@ def solve_puzzle_07_01(filepath):
 
     for equation in equations_list:
         outcome = equation.pop(0)
-        potential_outcomes = calculate_outcomes(equation)
-        if outcome in potential_outcomes:
-            outcomes_sum += outcome
-    
-    return outcomes_sum
-
-
-def solve_puzzle_07_02(filepath):
-    """ Create a list from a text file, identify all possible outcomes
-        including concatenation, and calculate their sum. """
-    
-    equations_list = list_equations(filepath)
-
-    outcomes_sum = 0
-
-    for equation in equations_list:
-        outcome = equation.pop(0)
-        potential_outcomes = calculate_outcomes(equation, concat=True)
+        potential_outcomes = calculate_outcomes(equation, concat=second)
         if outcome in potential_outcomes:
             outcomes_sum += outcome
     
@@ -96,11 +90,13 @@ def solve_puzzle_07_02(filepath):
 def main():
     """ Execute the main functions with the main input. """
 
-    result_1 = solve_puzzle_07_01(filepath)
-    print(f"Solution of the first puzzle of day 7: {result_1}.")
+    result_1, runtime_1 = solve_puzzle_07(filepath)
+    print(f"Solution to the first puzzle of day 7: {result_1}\n"
+          f"Runtime: {runtime_1} seconds\n")
 
-    result_2 = solve_puzzle_07_02(filepath)
-    print(f"Solution of the second puzzle of day 7: {result_2}.")
+    result_1, runtime_1 = solve_puzzle_07(filepath, second=True)
+    print(f"Solution to the first puzzle of day 7: {result_1}\n"
+          f"Runtime: {runtime_1} seconds\n")
 
 
 if __name__ == '__main__':
